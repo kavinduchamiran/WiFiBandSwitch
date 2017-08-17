b398c20b94ca45d7642fb2deafebc1180b7d921e
Kavindu <Kavindu@DESKTOP-6QT8QV9>
2017-08-17 18:08:15 +0530

    Add project files.

---
+++ WiFiBandSwitch/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WiFiBandSwitch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ni = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cms = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.wifi24 = new System.Windows.Forms.ToolStripMenuItem();
+            this.wifi5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.cms.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ni
+            // 
+            this.ni.ContextMenuStrip = this.cms;
+            this.ni.Icon = ((System.Drawing.Icon)(resources.GetObject("ni.Icon")));
+            this.ni.Text = "WiFi Band Switcher";
+            this.ni.Visible = true;
+            this.ni.Click += new System.EventHandler(this.ni_Click);
+            // 
+            // cms
+            // 
+            this.cms.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wifi24,
+            this.wifi5,
+            this.toolStripSeparator1,
+            this.exit});
+            this.cms.Name = "cms";
+            this.cms.Size = new System.Drawing.Size(153, 98);
+            // 
+            // wifi24
+            // 
+            this.wifi24.Name = "wifi24";
+            this.wifi24.Size = new System.Drawing.Size(152, 22);
+            this.wifi24.Text = "Set to 2.4GHz";
+            this.wifi24.Click += new System.EventHandler(this.wifi24_Click);
+            // 
+            // wifi5
+            // 
+            this.wifi5.Name = "wifi5";
+            this.wifi5.Size = new System.Drawing.Size(152, 22);
+            this.wifi5.Text = "Set to 5GHz";
+            this.wifi5.Click += new System.EventHandler(this.wifi5_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // exit
+            // 
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(152, 22);
+            this.exit.Text = "Exit";
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WiFiBandSwitcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.cms.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon ni;
+        private System.Windows.Forms.ContextMenuStrip cms;
+        private System.Windows.Forms.ToolStripMenuItem wifi24;
+        private System.Windows.Forms.ToolStripMenuItem wifi5;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exit;
+    }
+}
+
---
+++ WiFiBandSwitch/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Management;
+
+namespace WiFiBandSwitch
+{
+    public partial class Form1 : Form
+    {
+        String key = @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}\0002";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        private void wifi24_Click(object sender, EventArgs e)
+        {
+            Registry.SetValue(key, "WirelessMode", "34");
+
+            ResetNetworkAdaptor();
+
+            connectWiFi("UoM_Wireless");
+        }
+
+        private void wifi5_Click(object sender, EventArgs e)
+        {
+            Registry.SetValue(key, "WirelessMode", "17");
+
+            ResetNetworkAdaptor();
+
+            connectWiFi("UoM_Wireless");
+        }
+
+        public void ResetNetworkAdaptor()
+        {
+            String query = "interface set interface \"Wi-Fi\" Disable";
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "netsh.exe";
+            startInfo.Arguments = query;
+            process.StartInfo = startInfo;
+            process.Start();
+
+            System.Threading.Thread.Sleep(2500);
+
+            query = "interface set interface \"Wi-Fi\" Enable";
+
+            process = new System.Diagnostics.Process();
+            startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "netsh.exe";
+            startInfo.Arguments = query;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        public void connectWiFi(String name)
+        {
+            System.Threading.Thread.Sleep(1000);
+            String query = "wlan connect name=" + name;
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "netsh.exe";
+            startInfo.Arguments = query;
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void ni_Click(object sender, EventArgs e)
+        {
+
+           // cms.Show(this, Control.MousePosition); // Or any other overload
+            cms.Show(Control.MousePosition);
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
